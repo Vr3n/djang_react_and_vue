c22e59fcb85d7fef65d9fe4c27d172bab99e635d
vr3n <vickyspatel@gmail.com>
2021-02-24 20:57:05 +0530

    Integrated React with Django

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,21 @@
-import _ from 'lodash';
+// Vanilla Js.
+// import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'lodash'], '');
+//     element.innerHTML = _.join(['Hello', 'lodash'], '');
 
-    return element;
-}
+//     return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+// React JS.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello from React</h1>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
     output: {
         filename: 'index-bundle.js', // Output bundle file name
         path: path.resolve(__dirname, './static'),  // path to our django static directory
-    }
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    "@babel/preset-env",
+                    "@babel/preset-react"
+                ]
+            }
+        }],
+    },
 }
